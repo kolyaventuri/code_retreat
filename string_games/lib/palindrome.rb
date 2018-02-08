d13d47d687bea0fffb5debd869bb032380ac07f8
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-08 14:28:32 -0700

    Made palindrome class methods 3 lines long

--- string_games/lib/palindrome.rb
+++ string_games/lib/palindrome.rb
@@ -1,10 +1,14 @@
 class Palindrome
 
   def check(word)
-    word.gsub!(" ", "")
-    (word.length/2+1).times do |index|
+    word.delete!(' ')
+    return true if palindrome?(word).instance_of?(Integer)
+    false
+  end
+
+  def palindrome?(word)
+    (word.length / 2 + 1).times do |index|
       return false if word[index] != word[-1 - index]
     end
-    true
   end
 end
