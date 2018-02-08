a9658b27747e6fbcd916c307e7ddbfb8c3afe2b5
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-08 15:02:26 -0700

    Initial sentence reversal code

--- string_games/test/sentence_reversal_test.rb
+++ string_games/test/sentence_reversal_test.rb
@@ -1,14 +1,14 @@
 require_relative 'test_helper'
-
+require 'pry'
+require './lib/sentence_reversal.rb'
 class SentenceReversalTest < Minitest::Test
-	
+
 	def setup
-		@string_reversal = StringReversal.new
+		@sentence_reversal = SentenceReversal.new
 	end
 
 	def test_does_reverse_text
-		assert_equal "cat the", @string_reversal.reverse("the cat")
-		assert_equal "project a is this", @string_reversal.reverse("this is a project")"	
+		assert_equal "cat the", @sentence_reversal.reverse("the cat")
 	end
 
 end
