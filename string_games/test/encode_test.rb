39a1505d3c7bc1b2a580ef7868f126dfd5647280
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-08 15:33:48 -0700

    Encoder tests

---
+++ string_games/test/encode_test.rb
@@ -0,0 +1,19 @@
+require_relative 'test_helper.rb'
+
+require './lib/encoder.rb'
+
+class EncoderTest < Minitest::Test
+  def setup
+    @encoder = Encoder.new
+  end
+
+  def test_can_encode_string
+    assert_equal '4a5b6c', @encoder.encode('aaaabbbbbcccccc')
+    assert_equal 'ab3c3a', @encoder.encode('abcccaaa')
+  end
+
+  def test_can_decode_string
+    assert_equal 'aaaabbbbbcccccc', @encoder.decode('4a5b6c')
+    assert_equal 'abcccaaa', @encoder.decode('ab3c3a')
+  end
+end
